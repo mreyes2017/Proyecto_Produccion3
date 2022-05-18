441b34065367840a502fda069ca3ab1634837f5d
mreyesy17c2 <moisesreyes609@gmail.com>
2022-05-17 21:25:07 -0600

    login bd funcional

--- application.py
+++ application.py
@@ -4,6 +4,7 @@ from flask_session import Session
 from tempfile import mkdtemp
 from werkzeug.exceptions import default_exceptions
 from werkzeug.security import check_password_hash, generate_password_hash
+from helpers import apology, login_required
 
 app = Flask(__name__)
 
@@ -43,7 +44,7 @@ def login():
             return apology("Escribe una contraseña valida", 403)
 
         # Query database for username
-        rows = db.execute("SELECT * FROM users WHERE username = :username",
+        rows = db.execute("SELECT * FROM users WHERE email = :username",
                           username=request.form.get("username"))
 
         # Ensure username exists and password is correct
