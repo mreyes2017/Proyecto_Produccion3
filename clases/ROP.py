3e6b0a22657715687dd14e72d39a534533482fd0
willigm <58922780+willigm@users.noreply.github.com>
2022-05-19 12:11:03 -0600

    Update ROP.py
    actualizando la clase rop

--- clases/ROP.py
+++ clases/ROP.py
@@ -1,20 +1,20 @@
+from math import sqrt
 class ROP:
-    def __init__(self,demanda,dias_laborales,plazo_entrega):
+    def __init__(self,demanda,plazo_entrega):
         self.demanda = demanda
-        self.dias_laborales = dias_laborales
         self.plazo_entrega = plazo_entrega
         
 
     def calcular_rop(self):
         demanda_por_dia = float(self.demanda / 360)
-        rop = float(demanda_por_dia*self.plazo_entrega)
+        rop = round(float(demanda_por_dia*self.plazo_entrega),2) 
         return rop 
 
 
 #prueba xd
 
-rop_p = ROP(1500000,360,2)
-ROP=rop_p.calcular_rop()
+#rop_p = ROP(1500000,360,2)
+#ROP=rop_p.calcular_rop()
 
 #punto de reorden con desviacion estandar
 
@@ -23,12 +23,14 @@ class ROP_ESTANDAR:
         self.demanda_por_dia= demanda_por_dia
         self.plazo_entrega= plazo_entrega
         self.desviacion=desviacion
+        self.z=z
 
     def calcular_desviacion(self):
         Rl= self.desviacion*sqrt(self.plazo_entrega)
         rop_estandar= float(self.demanda_por_dia*self.plazo_entrega)+(self.z*Rl)
-        return rop_estandar
+        return round(rop_estandar,2)
 #prueba x2
 
 rop_estandar=ROP_ESTANDAR(200,4,1.65,150)
 ROP_ESTANDAR= rop_estandar.calcular_desviacion()
+
