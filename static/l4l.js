2eda51c60d8a4c79f5ccf1087fbfefbe52254d52
william <williamgm2011@gmail.com>
2022-05-24 14:14:08 -0600

    mejora .3

--- static/l4l.js
+++ static/l4l.js
@@ -1,13 +1,15 @@
 let cantidad_semanas=document.getElementById('cantidad_semanas');
 const formulario=document.querySelector('#prueba');
 const c=document.querySelector("#cantidad_semanas");
-const demanda= document.getElementById('demanda');
+const demanda= document.getElementById('demanda[]');
     
    // método js que se dispara al momento que el usuario toca la tecla enter
    c.onkeydown = function(e){
     //var ev = document.all ? window.event : e;
     if(e.keyCode==13) {
+        //desabilito el input donde ingrese la cntidad de semanas
        const x=document.querySelector("#cantidad_semanas").setAttribute("disabled",'disabled');
+       //habilito el input 
        demanda.disabled=false;
       
     }
@@ -16,20 +18,18 @@ const demanda= document.getElementById('demanda');
 
     demanda.onkeydown=function(event) {
        if (event.keyCode == 13) {
-           alert("estamos aqui");
+       // alert(cantidad_semanas.value);
+       demanda.disabled=false;
+        
+           
+               
        }
    };
  
- 
+   
 
 
 
-function ingresar_datos() {
-savearray = [];
-var array = [];  
-    for (var i = 0; i < cantidad_semanas.value; i++) {               
-     array[i] = parseFloat(document.getElementById("demanda"+(i+1)).value);
-            savearray.push (array[i]);  
-     }     
-}         
+
+
  
