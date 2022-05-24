d48590f904c13489bc0dbe1f382b6fa92dceb142
william <williamgm2011@gmail.com>
2022-05-24 12:09:50 -0600

    mejorando js bugs del enter

--- static/l4l.js
+++ static/l4l.js
@@ -1,6 +1,7 @@
 let cantidad_semanas=document.getElementById('cantidad_semanas');
 const formulario=document.querySelector('#formulario');
 const c=document.querySelector("#cantidad_semanas");
+
     
    // método js que se dispara al momento que el usuario toca la tecla enter
    c.onkeydown = function(e){
@@ -14,11 +15,15 @@ const c=document.querySelector("#cantidad_semanas");
 function agregar_input(){
    formulario.innerHTML += "<label >Digite la demanda:</label><div class='field'><input type='number' id='demanda' name='demanda' required/></div>";
   }
+  const demanda= document.querySelector("#demanda");
+     demanda.keypress(function(event) {
+        if (event.keyCode == 13) {
+            alert("estamos aqui");
+        }
+    });
 
-const demanda= document.getElementById("demanda"); 
-demanda.addEventListener("onfocus",function(){
-ingresar_datos();
-});
+
+
 function ingresar_datos() {
 savearray = [];
 var array = [];  
