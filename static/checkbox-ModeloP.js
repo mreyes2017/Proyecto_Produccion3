3ebeccb3046dcc89fc546d61e7720d7d44aca17f
willigm <58922780+willigm@users.noreply.github.com>
2022-05-26 08:21:23 -0600

    Delete static directory

--- static/checkbox-ModeloP.js
+++
@@ -1,6 +0,0 @@
-const inputsDesv = document.querySelector("#CDesvEstandar");
-const cuadroDesv = document.querySelector("#DesvEstandar");
-
-cuadroDesv.addEventListener("click", function () {
-  inputsDesv.toggleAttribute("disabled");
-});
--- static/checkbox-modeloQ.js
+++
@@ -1,6 +0,0 @@
-const inputsCosto = document.querySelector("#checkeadoTasa");
-const cuadroTasa = document.querySelector("#checkTasa");
-
-cuadroTasa.addEventListener("click", function () {
-  inputsCosto.toggleAttribute("disabled");
-});
